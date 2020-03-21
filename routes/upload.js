71e480866b892da54a9bbebcaf0480a237ad0ff9
best <best@developer.com>
2020-03-21 21:40:06 +0100

    added webpage structure

--- routes/upload.js
+++ routes/upload.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* POST a shopping list */
 router.post('/', function(req, res, next) {
   storage.push(req.body);
-	res.json('OK');
+	res.sendFile('success.html', {root: __dirname });
 });
 
 module.exports = router;
