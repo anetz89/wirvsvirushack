0da84f4b9e034bc49854c623fc6fce045b787834
Markus Anetsberger <markus.anetsberger@bmw.de>
2020-03-22 17:24:25 +0100

    first map release

--- routes/storage.js
+++ routes/storage.js
@@ -1,5 +1,29 @@
 var storage = {
-  list: []
+  list: [{
+      'name': 'Markus',
+      'phoneNumber': '345678',
+      'shoppingItems': [
+          'Apfel',
+          'Schokolade'
+      ],
+      'address': 'woanders, München',
+      'latlng': [
+          48.34369,
+          11.24907
+      ]
+  }, {
+      'name': 'Daniel',
+      'phoneNumber': '345678',
+      'shoppingItems': [
+          'Apfel',
+          'Karotte'
+      ],
+      'address': 'Arnulfstraße 60, München',
+      'latlng': [
+          48.14369,
+          11.54907
+      ]
+  }]
 }
 
 
--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,33 @@
 var express = require('express');
+var http = require('http');
 var storage = require('./storage');
 var router = express.Router();
 
 /* POST a shopping list */
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  storage.push(req.body);
-  res.json("OK");
+  const listItem = req.body;
+  const url = 'http://geocode.xyz/?scantext=' + encodeURI(req.body.address) + '&geoit=JSON&region=DE';
+  console.log(url);
+  http.get(url, (httpresult) => {
+    var body = '';
+
+    httpresult.on('data', function(chunk){
+        body += chunk;
+    });
+
+    httpresult.on('end', function(){
+        var result = JSON.parse(body);
+
+        if (result.match && result.match.length) {
+          listItem.latlng = [parseFloat(result.match[0].latt), parseFloat(result.match[0].longt)]
+        } else {
+          listItem.latlng = ["48.1", "11.5"];
+        }
+
+        storage.push(listItem);
+        res.json("OK");
+    });
+  });
 });
 
 module.exports = router;
